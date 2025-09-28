71917a2a40cd91e470924a5fb52b645193b04347
Oli Legat <olivier.legat@ag-grid.com>
2025-09-28 19:29:03 +0300

    Add HageCoreRule to hagaCore

---
+++ toolchain/hagaCore.test.ts
@@ -0,0 +1,23 @@
+import { writeNinjaBuild, HagaCoreExport } from "./hagaCore";
+import { writeFileSync } from "node:fs";
+
+const core: HagaCoreExport = {
+  rules: [
+    {
+      name: "cc",
+      commands: [["gcc", "-c", "$in", "-o", "$out"]],
+      description: "Compiling $in"
+    },
+    {
+      name: "link",
+      commands: [["gcc", "$in", "-o", "$out"], ["echo", "Link complete!"]],
+      description: "Linking $out"
+    }
+  ],
+  targets: [
+    { inputs: ["foo.c"], outputs: ["foo.o"], rule: "cc" },
+    { inputs: ["foo.o"], outputs: ["foo"], rule: "link" }
+  ]
+};
+
+writeNinjaBuild(core, s => writeFileSync("build.ninja", s, { flag: "a" }));
